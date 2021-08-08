beae3272c46a0ff8780ddbcee3a6c5a30e858122
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-08 14:07:57 +0800

    riva: rootdir: Set permission on IR device node

--- device.mk
+++ device.mk
@@ -15,6 +15,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_sku2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku2.xml
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.xiaomi.device.rc
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
